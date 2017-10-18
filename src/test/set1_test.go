951230f73401518fd5216a993c04cf2bbabd3d9b
Tejas163 <tejaskrshna@gmail.com>
2017-10-18 12:31:24 +0530

    changed from test to src/test

---
+++ src/test/set1_test.go
@@ -0,0 +1,16 @@
+package	cryptopals
+
+import (
+	"testing"
+)
+func Test1(t *testing.T){
+
+res, err :=hextoBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+if err != nil{
+	t.Fatal(err)
+}
+if res != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"{
+	t.Fatal("wrong string",res)
+}
+
+}
