88dd2ad782e2febf62ca8097b8fa88fe1160bfa8
Tejas163 <tejaskrshna@gmail.com>
2017-10-19 19:35:33 +0530

    changed set1-problem 2

--- src/set1.go
+++ src/set1.go
@@ -6,11 +6,22 @@ import (
 	"log"
 )
 
-func hextoBase64(s string) (string, error)  {
+func hextoBase64(s string) (string, error) {
 	val, err := hex.DecodeString(s)
-	if err !=nil {
-		return "",err
+	if err != nil {
+		return "", err
 	}
-	log.Printf("%s",val)
+	log.Printf("%s", val)
 	return base64.StdEncoding.EncodeToString(val), nil
+}
+
+func xor(a, b []byte) []byte {
+	if len(a) != len(b) {
+		panic("xor mismatch length")
 	}
+	res := make([]byte, len(a))
+	for i := range a {
+		res[i] = a[i] ^ b[i]
+	}
+	return res
+}
